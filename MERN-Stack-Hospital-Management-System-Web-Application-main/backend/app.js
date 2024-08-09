c2f9affed7c814a83bd905908b16bb6a40a7dbe3
suhail5746 <suhailbdn2002@gmail.com>
2024-08-09 19:41:44 +0530

    first commit

---
+++ MERN-Stack-Hospital-Management-System-Web-Application-main/backend/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+import dotenv from "dotenv";
+
+const app = express();
+config({ path: "./config.env" });
+dotenv.config();
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+app.use(cors( {origin:allowedOrigins,credentials: true,}))
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
